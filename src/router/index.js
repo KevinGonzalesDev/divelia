e7f181122e0cd28a2b4a770b36604c127fd5faba
Kevin Gonzales Marin <kevinpiero00@gmail.com>
2023-07-13 19:41:40 -0500

    changes for vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,11 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   component: HomeView,
+  // },
   {
     path: "/about",
     name: "about",
@@ -18,7 +18,7 @@ const routes = [
   },
 
   {
-    path: "/login",
+    path: "/",
     name: "login",
     component: () => import("../views/login.vue"),
   },
